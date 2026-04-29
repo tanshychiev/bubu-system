e60130d484f2f18e9c2dafd7e51feeb17fc6f8b5
Tanshychiev <YOUR_EMAIL_HERE>
2026-04-29 18:07:05 +0700

    fix static serve for bubu

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
 ]
 
+# 🔥 FIX STATIC + MEDIA
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
